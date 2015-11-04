f304214a413f91f4d81ca4232a8b30949275d6e8
Kirill Kayumov <kirill.kayumov@flatstack.com>
2015-11-04 19:56:08 +0300

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  include Authentication
+  include Authorization
+  include MiniProfilerAuthorization
+
+  protect_from_forgery with: :exception
+  responders :flash
+
+  decent_configuration do
+    strategy DecentExposure::StrongParametersStrategy
+  end
+end
