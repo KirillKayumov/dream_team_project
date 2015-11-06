f2f65fbcaa3cd7d8e245f221d3cc30a25100be8f
Kirill Kayumov <kirill.kayumov@flatstack.com>
2015-11-06 10:20:13 +0300

    Add reactions to comments

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -4,12 +4,21 @@ class ReactionsController < ApplicationController
   def create
     reaction.user = current_user
     reaction.save
-    redirect_to post_path(params[:reaction][:reactive_id])
+    redirect_to object
   end
 
   private
 
   def reaction_params
     params.require(:reaction).permit(:reactive_id, :reactive_type, :positive)
+  end
+
+  def object
+    case params[:reaction][:reactive_type]
+    when "Post"
+      Post.find(params[:reaction][:reactive_id])
+    when "Comment"
+      Comment.find(params[:reaction][:reactive_id]).post
+    end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  has_many :reactions, as: :reactive
 
   validates :text, :user, :post, presence: true
 
